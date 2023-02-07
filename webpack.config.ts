f7be67c6a5d09c5ff1eac55b212f59d4e4b601b7
LisitsaP <lisitsa.p.e@gmail.com>
2023-02-07 18:16:01 -0500

    added css, scss loader and react

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,18 +5,18 @@ import path from "path";
 
 export default (env: BuildEnv) => {
   const paths: BuildPaths = {
-    entry: path.resolve(__dirname, "src", "index.ts"),
+    entry: path.resolve(__dirname, "src", "index.tsx"),
     build: path.resolve(__dirname, "build"),
     html: path.resolve(__dirname, "public", "index.html"),
   };
 
   const mode = env.mode || "development";
-  const PORT = env.port || 3000;
-
   const isDev = mode === "development";
 
+  const PORT = env.port || 3006;
+
   const config: webpack.Configuration = buildWebpackConfig({
-    mode: mode,
+    mode,
     paths,
     isDev,
     port: PORT,
