5eb2c5d75a1d52a7573de6aeb392ae14961ca2fc
LisitsaP <lisitsa.p.e@gmail.com>
2023-02-10 18:57:16 -0500

    added react routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import Counter from "./components/Counter";
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./index.scss";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
 
 export default function App() {
   return (
     <div className="app">
-      Hello
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Routes>
+        <Route path={"/about"} element={<AboutPage />} />
+        <Route path={"/"} element={<MainPage />} />
+      </Routes>
     </div>
   );
 }
