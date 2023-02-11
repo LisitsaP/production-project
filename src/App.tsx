0163169aa79e4dfa4e1f745d36e582b3439c5982
LisitsaP <lisitsa.p.e@gmail.com>
2023-02-10 22:58:24 -0500

    splitting Lazy Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import "./index.scss";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 export default function App() {
   return (
     <div className="app">
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
-      <Routes>
-        <Route path={"/about"} element={<AboutPage />} />
-        <Route path={"/"} element={<MainPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loadin...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
