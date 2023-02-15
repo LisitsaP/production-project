3e0c15e53b6ce51e4038e80ac7868094b2245b79
LisitsaP <lisitsa.p.e@gmail.com>
2023-02-15 11:44:13 -0500

    added route config, moved router to app/providers

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,8 @@
-import React, { Suspense, useContext } from "react";
 import { Link } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
-import { useState } from "react";
-import {
-  ThemeContext,
-  Theme,
-} from "./providers/ThemeProvider/lib/ThemeContext";
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
+import { AppRouter } from "./providers/router";
 
 export default function App() {
   const { theme, toggleTheme } = useTheme();
@@ -19,12 +11,7 @@ export default function App() {
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>Loadin...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 }
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import { routeConfig } from "shared/routeConfig/routeConfig";
+
+export default function AppRouter() {
+  return (
+    <Suspense fallback={<div>Loadin...</div>}>
+      <Routes>
+        {Object.values(routeConfig).map(({ element, path }) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <Suspense fallback={<div>Loadin...</div>}>{element}</Suspense>
+            }
+          />
+        ))}
+      </Routes>
+    </Suspense>
+  );
+}
