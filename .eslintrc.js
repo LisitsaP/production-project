892f4905312418da97e953ce6dfc2b5f8431cbe1
LisitsaP <lisitsa.p.e@gmail.com>
2023-03-09 17:15:53 -0500

    added jest, added first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     settings: {
         react: {
@@ -31,7 +32,8 @@ module.exports = {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'react/jsx-filename-extension': [2,
+            { extensions: ['.js', '.jsx', '.tsx'] }],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'warn',
@@ -46,6 +48,8 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'react/prop-types': 'warn',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'linebreak-style': 'off',
+        'max-len': ['error', { ignoreComments: true }],
 
     },
     globals: {
