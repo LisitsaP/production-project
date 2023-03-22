38d400dec09072926658b609c78567607a3b2d15
LisitsaP <lisitsa.p.e@gmail.com>
2023-03-22 16:14:27 -0500

    set up storybook and decorators

---
+++ src/shared/ui/Loader/Loader.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import 'app/styles/index.scss';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/TemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Loader} from "./Loader"
+
+export default {
+  title: 'widgets/Loader',
+  component: Loader,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  args: {
+    to: '/',
+  }
+} as ComponentMeta<typeof Loader>;
+
+const Template: ComponentStory<typeof Loader> = (args) => <Loader {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+
